1dea543089016b9b412f5f054e230288f8e46283
nikulson <nikulshin@mail.ru>
2022-10-21 20:51:11 +0400

    d7

--- news/views.py
+++ news/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.views.generic import TemplateView, ListView, DetailView
-from .models import Post
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from .models import Post, Author, Category, User
 from datetime import datetime
+from .filters import PostFilter
+from .forms import PostForm
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
 
 
 class BaseView(TemplateView):
@@ -22,19 +28,98 @@ class PostsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
         context_object_name = 'posts_list'
+        paginate_by = 2
 
+
+        def get_queryset(self):
+            queryset = super().get_queryset()
+            self.filterset = PostFilter(self.request.GET, queryset)
+            return self.filterset.qs
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
             context['time_now'] = datetime.utcnow()
             context['value1'] = None
+            context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+            context['categories'] = Category.objects.all()
+            context['authors'] = Author.objects.all()
+            context['form'] = PostForm()
+            context['filterset'] = self.filterset
             return context
 
 
 class PostDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Post
-    # Используем другой шаблон — product.html
+    # Используем другой шаблон — post.html
     template_name = 'post.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'search'
+    queryset = Post.objects.order_by('-post_pub_date')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class PostCreate(PermissionRequiredMixin, CreateView):
+    model = Post
+    template_name = 'create.html'
+    context_object_name = 'create'
+    queryset = Post.objects.order_by('-post_pub_date')
+    paginate_by = 10
+    form_class = PostForm  # добавляем форм класс, чтобы получать доступ к форме через метод POST
+    permission_required = ('news.create_post',)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['categories'] = Category.objects.all()
+        context['authors'] = Author.objects.all()
+        context['form'] = PostForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)  # создаём новую форму, забиваем в неё данные из POST-запроса
+
+        if form.is_valid():  # если пользователь ввёл всё правильно и нигде не ошибся, то сохраняем новый пост
+            form.save()
+
+        return super().get(request, *args, **kwargs)
+
+
+
+class PostEditView(PermissionRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'create.html'
+    form_class = PostForm
+    permission_required = ('news.post_edit',)
+
+    #  метод get_object мы используем вместо queryset, чтобы получить информацию об объекте, который мы собираемся редактировать
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+
+class PostDeleteView(PermissionRequiredMixin, DeleteView):
+    template_name = 'delete.html'
+    queryset = Post.objects.all
+    success_url = '/posts/'
+    permission_required = ('news.post_delete',)
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
